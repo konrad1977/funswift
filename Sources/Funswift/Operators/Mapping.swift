f13d15fbb9216a7ae81ee899ee18a751dcc5d5e3
Mikael Konradsson <mikael.konradsson@outlook.com>
2021-04-11 10:38:17 +0200

    Add mapping

--- Sources/Funswift/Operators/Mapping.swift
+++ Sources/Funswift/Operators/Mapping.swift
@@ -8,58 +8,122 @@ import Foundation
 precedencegroup MapCompose { associativity: left higherThan: Pipe }
 infix operator <&>: MapCompose
 
-public func <&> <A, B, C>(
+// MARK:- Optionals
+public func <&><A, B, C>(
     _ f: @escaping (A) -> B?,
     _ g: @escaping (B) -> C
 ) -> (A) -> C? {
     return { f($0).map(g) }
 }
 
-public func <&> <A, B, C>(
+public func <&><A, B>(
+	_ lhs: A?,
+	_ g: @escaping (A) -> B
+) -> B? {
+	return { lhs.map(g) }()
+}
+
+// MARK:- Result
+public func <&><A, B, C>(
     _ f: @escaping (A) -> Result<B, Error>,
     _ g: @escaping (B) -> C
 ) -> (A) -> Result<C, Error> {
     return { f($0).map(g) }
 }
 
-public func <&> <A, B, C>(
+public func <&><A, B>(
+	_ lhs: Result<A, Error>,
+	_ g: @escaping (A) -> B
+) ->  Result<B, Error> {
+	return { lhs.map(g) }()
+}
+
+// MARK:- Arrays
+public func <&><A, B, C>(
     _ f: @escaping (A) -> [B],
     _ g: @escaping (B) -> C
 ) -> (A) -> [C] {
     return { f($0).map(g) }
 }
 
-public func <&> <A, B, C>(
+public func <&><A, B>(
+	_ lhs: [A],
+	_ g: @escaping (A) -> B
+) -> [B] {
+	return { lhs.map(g) }()
+}
+
+// MARK:- Changeable
+public func <&><A, B, C>(
     _ f: @escaping (A) -> Changeable<B>,
     _ g: @escaping (B) -> C
 ) -> (A) -> Changeable<C> {
     return { f($0).map(g) }
 }
 
-public func <&> <A, B, C>(
+public func <&><A, B>(
+	_ lhs: Changeable<A>,
+	_ g: @escaping (A) -> B
+) -> Changeable<B> {
+	return { lhs.map(g) }()
+}
+
+// MARK:- IO
+public func <&><A, B, C>(
     _ f: @escaping (A) -> IO<B>,
     _ g: @escaping (B) -> C
 ) -> (A) -> IO<C> {
     return { f($0).map(g) }
 }
 
-public func <&> <A, B, C>(
+public func <&><A, B>(
+	_ lhs: IO<A>,
+	_ g: @escaping (A) -> B
+) -> IO<B> {
+	return { lhs.map(g) }()
+}
+
+// MARK:- Deferred
+public func <&><A, B, C>(
     _ f: @escaping (A) -> Deferred<B>,
     _ g: @escaping (B) -> C
 ) -> (A) -> Deferred<C> {
     return { f($0).map(g) }
 }
 
-public func <&> <A, B, C, Environment>(
+public func <&><A, B>(
+	_ lhs: Deferred<A>,
+	_ g: @escaping (A) -> B
+) -> Deferred<B> {
+	return { lhs.map(g) }()
+}
+
+// MARK:- Reader
+public func <&><A, B, C, Environment>(
     _ f: @escaping (A) -> Reader<Environment, B>,
     _ g: @escaping (B) -> C
 ) -> (A) -> Reader<Environment, C> {
     return { f($0).map(g) }
 }
 
-public func <&> <A, B, C, S>(
+public func <&><A, B, Environment>(
+	_ lhs: Reader<Environment, A>,
+	_ g: @escaping (A) -> B
+) -> Reader<Environment, B> {
+	return { lhs.map(g) }()
+}
+
+// MARK:- State
+public func <&><A, B, C, S>(
     _ f: @escaping (A) -> State<S, B>,
-    _ g: @escaping (B) -> C
+	_ g: @escaping (B) -> C
 ) -> (A) -> State<S, C> {
     return { f($0).map(g) }
+}
+
+public func <&><A, B, S>(
+	_ lhs: State<S, A>,
+	_ g: @escaping (A) -> B
+) -> State<S, B> {
+	return { lhs.map(g) }()
 }
