0a2d66c85dea2bdc5d412fc4c5853e86cd81c703
Mikael Konradsson <mikael.konradsson@outlook.com>
2021-04-13 20:40:40 +0200

    Add more tests for <&> operator

--- Tests/FunswiftTests/OperatorFunctor.swift
+++ Tests/FunswiftTests/OperatorFunctor.swift
@@ -3,18 +3,39 @@ import XCTest
 
 final class OperatorFunctor: XCTestCase {
 
+	func incr(_ value: Int) -> Int { value + 1 }
+	func uppercased(_ value: String) -> String { value.uppercased() }
+
     func testArrayMap() {
-        let result = [1,2,3] <&> { $0 + 1 }
+        let result = [1,2,3] <&> incr
         XCTAssertEqual([2,3,4], result)
     }
 
     func testResultMap() {
-        let result = Result.success(10) <&> { $0 + 1 }
+        let result = Result.success(10) <&> incr
         try XCTAssertEqual(11, result.get())
     }
 
     func testIOMap() {
-        let result = IO { "Hello world" } <&> { $0.uppercased() }
+        let result = IO { "Hello world" } <&> uppercased
         XCTAssertEqual("HELLO WORLD", result.unsafeRun())
     }
+
+	func testDefferedMap() {
+		let deferred = Deferred { $0("Hello world") } <&> uppercased
+		deferred.run { result in
+			XCTAssertEqual("HELLO WORLD", result)
+		}
+	}
+
+	func testReaderMap() {
+		let reader = Reader<Void, String> { "Hello world" } <&> uppercased
+		XCTAssertEqual("HELLO WORLD", reader.run(()))
+	}
+
+	func testChangeableMap() {
+		let changeable = Changeable("Hello world") <&> uppercased
+		XCTAssertEqual("HELLO WORLD", changeable.value)
+		XCTAssertFalse(changeable.hasChanges)
+	}
 }
