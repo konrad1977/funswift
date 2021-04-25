d1af841518d1ba6f178aac5ab5308154e5e66114
Mikael Konradsson <mikael.konradsson@outlook.com>
2021-04-25 09:48:10 +0200

    Add platform versions iOS(v9) and macOS(.v10_11)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Funswift",
+	platforms: [
+		.iOS(.v9),
+		.macOS(.v10_11)
+	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
